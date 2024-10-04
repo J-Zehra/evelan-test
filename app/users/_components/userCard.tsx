70e623b17be3497a82ba6a6bba16654ff5751831
Jad <jazencode@gmail.com>
2024-10-04 22:37:32 +0800

    optimize image

--- app/users/_components/userCard.tsx
+++ app/users/_components/userCard.tsx
@@ -9,28 +9,15 @@ export default function UserCard({ user }: Props) {
   return (
     <li>
       <article className={styles.card}>
-        <picture>
-          <source
-            media="(max-width:100px)"
-            srcSet={user.avatar}
-            type="image/webp"
-          />
-          <source
-            media="(max-width:130px)"
-            srcSet={user.avatar}
-            type="image/webp"
-          />{" "}
-          <Image
-            src={user.avatar}
-            alt="User Avatar"
-            width={30}
-            height={40}
-            sizes="100vw"
-            priority={false}
-            className={styles.avatar}
-          />
-        </picture>
-
+        <Image
+          src={user.avatar}
+          alt="User Avatar"
+          width={30}
+          height={40}
+          sizes="5rem"
+          priority={false}
+          className={styles.avatar}
+        />
         <h1 className={styles.user_id}>{user.id}</h1>
         <h1
           className={styles.user_name}
