e3e6e7e5c728394c777eb498233098c4674d6ec7
Jad <jazencode@gmail.com>
2024-10-04 22:31:46 +0800

    add picture tag

--- app/users/_components/userCard.tsx
+++ app/users/_components/userCard.tsx
@@ -9,15 +9,28 @@ export default function UserCard({ user }: Props) {
   return (
     <li>
       <article className={styles.card}>
-        <Image
-          src={user.avatar}
-          alt="User Avatar"
-          width={30}
-          height={40}
-          sizes="100vw"
-          priority={false}
-          className={styles.avatar}
-        />
+        <picture>
+          <source
+            media="(max-width:100px)"
+            srcSet={user.avatar}
+            type="image/webp"
+          />
+          <source
+            media="(max-width:130px)"
+            srcSet={user.avatar}
+            type="image/webp"
+          />{" "}
+          <Image
+            src={user.avatar}
+            alt="User Avatar"
+            width={30}
+            height={40}
+            sizes="100vw"
+            priority={false}
+            className={styles.avatar}
+          />
+        </picture>
+
         <h1 className={styles.user_id}>{user.id}</h1>
         <h1
           className={styles.user_name}
