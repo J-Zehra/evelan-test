29dfee872687d28345a6630cad349a350d23c95c
Jad <jazencode@gmail.com>
2024-10-10 23:39:29 +0800

    reformat to server side

--- app/_hooks/useFetchUsers.ts
+++ app/_hooks/useFetchUsers.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const BASE = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -7,15 +7,13 @@ export default function useFetchUsers(initialUsers: UserType[]) {
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
 
-  const handleLoadMore = () => {
+  const handleLoadMore = async () => {
     setPage((prev) => prev + 1);
-  };
 
-  useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const data = await fetch(`${BASE}/api/users?page=${page}`);
+        const data = await fetch(`${BASE}/api/users?page=${page + 1}`);
         const users = (await data.json()).data as UserType[];
         setUsers((prev) => [...prev, ...users]);
       } catch (error) {
@@ -25,7 +23,7 @@ export default function useFetchUsers(initialUsers: UserType[]) {
       }
     };
     fetchData();
-  }, [page]);
+  };
 
   return {
     users,
